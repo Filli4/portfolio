028e3da36fc44a347074e36de6c7f54b89da4136
filli4 <filmon.e@yahoo.com>
2024-03-21 18:39:06 +0100

    second

---
+++ app/page.js
@@ -0,0 +1,30 @@
+
+
+export default function Home() {
+  
+
+return (
+    <main className="">
+     <div className="flex-cols p-6 gap-x-6 justify-between flex items-center bg-slate-200">
+      <p className="flex ">Profile</p>
+      <div>
+        <input placeholder="search" className="bg-slate-300 rounded-3xl px-6"></input>
+      </div>
+      <nav className="flex gap-x-6 ">
+      <a className="flex">nav-items</a>
+      <a className="flex">nav-items</a>
+      <a className="flex">nav-items</a>
+      <a className="flex">nav-items</a>
+      </nav>
+      </div>
+
+      <div className=" h-72 bg-slate-300">
+        <div className="flex justify-start "><h1>soothing somthing</h1></div>
+        <p>somthing somthing somthing somthing somthing somthing somthing somthing somthing somthing </p>
+        <p>somthing somthing somthing somthing somthing somthing somthing somthing somthing somthing somthing somthing somthing somthing somthing somthing somthing somthing </p>
+        <h4></h4>
+      </div>
+    </main>
+  )
+}
+
