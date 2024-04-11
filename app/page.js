c535fb9fd27c05ee42653c4ec1512bf0220a2e19
Your Name <you@example.com>
2024-04-12 00:27:26 +0200

    social

--- app/page.js
+++ app/page.js
@@ -7,16 +7,24 @@ return (
     <main >
      <div className="grid grid-cols-3 p-7 justify-around gap-x-8 items-center bg-slate-200">
       <h1 className="grid justify-center">Profile</h1>
-        <input placeholder="search" className="bg-slate-300 grid rounded-3xl px-5"></input>
+       
       <nav className="grid  grid-cols-4 justify-center mx-4 ">
-      <a className="justify-center grid">home</a>
-      <a className="justify-center grid">about </a>
-      <a className="justify-center grid">contact </a>
-      <a className="justify-center grid">more</a>
+      <a href="" className="justify-center grid">home</a>
+      <a href="" className="justify-center grid">about </a>
+      <a href="" className="justify-center grid">contact </a>
+      <a href="" className="justify-center grid">more</a>
+      </nav>
+      <nav className="grid  grid-cols-3 w-fit justify-self-end gap-4 ">   
+      <a href="" className=" grid">Linkedin</a>
+      <a href="" className=" grid">github </a>
+      <a href="https://www.instagram.com/?next=%2F&hl=en" className=" grid">instagram</a>
       </nav>
       </div>
 
-      <div className=" grid grid-cols-4 p-2 gap-2  bg-slate-300 h-72">
+
+
+
+      {/* <div className=" grid grid-cols-4 p-2 gap-2  bg-slate-300 h-72">
         
         <div className="flex rounded-2xl p-4  flex-nowrap justify-center bg-slate-800 items-center flex-col">
         <h1 className="p-2 font-bold text-3xl text-white ">Card</h1>
@@ -42,7 +50,7 @@ return (
         <p className="p-2 font-normal text-slate-400 ">somthing somthing somthing somthing somthing somthing </p>
         </div>
 
-      </div>   
+      </div>    */}
     </main>
   )
 }
